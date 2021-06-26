2fb6323c65ec73eb76644afbdb6eb8499ce6ffad
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-06-26 12:33:25 +0100

    feat: products

--- client/src/app/dashboard/components/dashboard-product/dashboard-product.component.ts
+++ client/src/app/dashboard/components/dashboard-product/dashboard-product.component.ts
@@ -9,6 +9,40 @@ import {
 })
 export class DashboardProductComponent implements OnInit {
   starIcon = faStar;
+  products = [
+    {
+      name: 'Pepper soup',
+      image: 'pepper-soup-goat-meat.jpg'
+    },
+    {
+      name: 'Egusi soup',
+      image: 'egusi-soup.jpg'
+    },
+    {
+      name: 'cat pepper soup',
+      image: 'catfish-pepper-soup.jpg'
+    },
+    {
+      name: 'Water leaf',
+      image: 'water-leave-soup.jpg'
+    },
+    {
+      name: 'Fried Rice',
+      image: 'fried-rice.jpg'
+    },
+    {
+      name: 'Jollof Rice',
+      image: 'jollof-rice.webp'
+    },
+    {
+      name: 'Afang soup',
+      image: 'nigerian-afang-soup.jpg'
+    },
+    {
+      name: 'Pepper soup b',
+      image: 'pepper-soup.jpg'
+    }
+  ];
 
   constructor() { }
 

166e34a56997642ff4c978c5d1da65f21425a98b
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-09-11 14:54:56 +0100

    feat: create folder by server

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path');
 const express = require('express');
 const bodyParser = require( 'body-parser' );
 const cors = require('cors');
@@ -20,10 +22,20 @@ app.use('/a',express.static('/b'));
 Above line would serve all files/folders inside of the 'b' directory
 And make them accessible through http://localhost:3000/a.
 */
-const path = require('path');
 const dir = path.join(__dirname, 'public');
-
 app.use( express.static( dir ) )
+
+// create upload folder
+const folderName = `${dir}/uploads`
+
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName)
+  }
+} catch (err) {
+  console.error(err)
+}
+
 /*  */
 //router routes
 const productRouter = require('./server/router/ProductRouter')

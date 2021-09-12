74f3aab1d0c4f0a22c984c5a67cd9733f1a63e07
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-09-12 12:55:21 +0100

    feat: verify token working

--- server/router/ProductRouter.js
+++ server/router/ProductRouter.js
@@ -2,9 +2,10 @@
 const express = require( "express" );
 const router = express.Router();
 const {list, getOne, postProduct, updateProductDetails, deleteProduct} = require( './../controllers/ProductController' );
-const {upload, uploadSingle, uploadMultiple, postSingleImg,postMultipleImg} = require( './../controllers/MainController' );
+const { upload, uploadSingle, uploadMultiple, postSingleImg, postMultipleImg } = require('./../controllers/MainController');
+const {verifyToken} = require('./../middleware/auth.js')
 // route
-router.get( '/products', list);
+router.get( '/products', verifyToken, list);
 router.get( '/product/:id', getOne );
 router.get( '/products/:page', list );
 router.post( '/product', upload.any(), postProduct );
